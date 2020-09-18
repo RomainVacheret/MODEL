5ec4971edcaa14680454320807b55d33a7373d23
Romain <romain.vacheret@gmail.com>
2020-09-18 15:25:08 +0200

    player's getters and setters

--- src/main/java/fr/parisnanterre/miage/rps/model/Player.java
+++ src/main/java/fr/parisnanterre/miage/rps/model/Player.java
@@ -9,6 +9,7 @@ public class Player {
     private final String name;
     private int score;
     private final Collection<Play> plays;
+    private int playsIdx = 0;
 
     Player(String name, Collection<Play> plays) {
         this.name = name;
@@ -21,5 +22,21 @@ public class Player {
         this.score = 0;
         this.plays = new ArrayList<>();
         new Random().ints(20, 0, 3).forEach(integer -> this.plays.add(Play.valueOf(integer)));
+    }
+
+    public String getName() { return this.name; }
+
+    public int getNumberOfPlays() { return 20; }
+
+    public int getScore() { return this.score; }
+
+    public void setScore(int newScore) { this.score = newScore; }
+
+    // Desole pour cette fonction vraiment sale ... Je n'ai pas encore l'habitude de Java...
+    public Play getNextMove() { return (Play) ((List) this.plays).get(this.playsIdx++); }
+
+    public static void main(String[] args) {
+        Player p = new Player("Toto");
+        System.out.println(p.getNextMove());
     }
 }
