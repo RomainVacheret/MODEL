70c282787748fd7a36e6d487ac8fb26c48c2255f
Romain <romain.vacheret@gmail.com>
2020-09-18 14:28:10 +0200

    each dataprovider test failing

--- src/test/java/fr/parisnanterre/miage/rps/model/RockPaperScissorsTest.java
+++ src/test/java/fr/parisnanterre/miage/rps/model/RockPaperScissorsTest.java
@@ -32,17 +32,17 @@ public class RockPaperScissorsTest {
         return new Object[][] {{Play.PAPER, Play.SCISSORS}, {Play.ROCK, Play.PAPER}, {Play.SCISSORS, Play.ROCK}};
     }
 
-    @Test(dataProvider = "winProvider")
+    @Test(dataProvider = "tieProvider")
     public void testPlayWin(Play p1, Play p2) {
         assertEquals(rps.play(p1, p2), Result.WIN);
     }
 
-    @Test(dataProvider = "tieProvider")
+    @Test(dataProvider = "lostProvider")
     public void testPlayTie(Play p1, Play p2) {
         assertEquals(rps.play(p1, p2), Result.TIE);
     }
 
-    @Test(dataProvider = "lostProvider")
+    @Test(dataProvider = "winProvider")
     public void testPlayLost(Play p1, Play p2) {
         assertEquals(rps.play(p1, p2), Result.LOST);
     }
